f8a8dfae20cf3a1eeb215eb8cd9ad822ad848eef
Zach <60633052+Area-Turtle@users.noreply.github.com>
2021-11-26 02:43:04 -0800

    changed visualizer stuff

--- client/src/visualizers/Area-Turtle_Visualizer.tsx
+++ client/src/visualizers/Area-Turtle_Visualizer.tsx
@@ -12,12 +12,10 @@ export const Area_Turtle_Visualizer = new Visualizer(
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
     const dim = Math.min(width, height);
-
     p5.background(0, 0, 0, 255);
-
     p5.strokeWeight(dim * 0.01);
-    p5.stroke(255, 255, 255, 255);
-    p5.noFill();
+    p5.stroke(255);
+    //p5.noFill();
 
     const values = analyzer.getValue();
     p5.beginShape();
