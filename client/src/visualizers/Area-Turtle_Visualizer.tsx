8040643247be738b683ddb29daaf17bee2d22c8b
Zach <60633052+Area-Turtle@users.noreply.github.com>
2021-11-27 02:10:30 -0800

    added vis bar stuff

--- client/src/visualizers/Area-Turtle_Visualizer.tsx
+++ client/src/visualizers/Area-Turtle_Visualizer.tsx
@@ -11,21 +11,34 @@ export const Area_Turtle_Visualizer = new Visualizer(
   (p5: P5, analyzer: Tone.Analyser) => {
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
-    const dim = Math.min(width, height);
-    p5.background(0, 0, 0, 255);
-    p5.strokeWeight(dim * 0.01);
-    p5.stroke(255);
-    //p5.noFill();
+    //const dim = Math.min(width, height);
+    p5.background(153,153,0);
+    //p5.strokeWeight(dim * 0.01);
+    p5.noFill();
 
     const values = analyzer.getValue();
-    p5.beginShape();
-    for (let i = 0; i < values.length; i++) {
-      const amplitude = values[i] as number;
-      const x = p5.map(i, 0, values.length - 1, 0, width);
-      const y = height /2 + amplitude * height;
+    for (var j = 0; j <55; j++){
+      //var r = p5.map(p5.sin(j),-1,1,0,225);
+      var r = p5.random(0,(p5.map(p5.sin(j),-1,1,0,225)));
+      var g = p5.map(j,0,20,0,255);
+      //var g = p5.random(0,(p5.map(j,0,20,0,255)));
+      var b = p5.map(p5.cos(j),-1,1,225,0);
+      //var b = p5.random(0,(p5.map(p5.cos(j),-1,1,225,0)));
+      p5.stroke(r,g,b);
+      let space_between_lines = width / 128;
+
+      p5.beginShape();
+      for (let i = 0; i < values.length; i++) {
+        const amplitude = values[i] as number;
+        p5.fill(r,g,b);
+        let x = p5.map(i, 0, values.length - 1, 0, width);
+        let y = (height + amplitude * height*p5.cos(i))-window.innerHeight/5;
+        //let y = p5.map(amplitude, 0, 256, height/2, 0);
       // Place vertex
-      p5.vertex(x, y);
+        //p5.vertex(x, y);
+        p5.rect(width - (i * space_between_lines), height-y, space_between_lines, height - y);
+      }
+      p5.endShape();
     }
-    p5.endShape();
   },
 );
