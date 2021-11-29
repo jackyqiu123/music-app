e3f56a1b3df03892f806ec80388e3925de61c0ac
Zach <60633052+Area-Turtle@users.noreply.github.com>
2021-11-29 03:06:36 -0800

    vis final done

--- client/src/visualizers/Area-Turtle_Visualizer.tsx
+++ client/src/visualizers/Area-Turtle_Visualizer.tsx
@@ -5,7 +5,7 @@ import * as Tone from 'tone';
 // project imports
 import { Visualizer } from '../Visualizers';
 
-
+let phase = 0
 export const Area_Turtle_Visualizer = new Visualizer(
   'Area-Turtle_Visualizer',
   (p5: P5, analyzer: Tone.Analyser) => {
@@ -27,10 +27,12 @@ export const Area_Turtle_Visualizer = new Visualizer(
       var x2 = 0
       for(var x = 0;x<cols;x++){
         var r = p5.noise(x2,y2)*255;
+        var g = p5.noise(x2,y2)*255;
+        var b = p5.noise(x2,y2)*255;
         var v = p5.createVector(0);
         x2 += inc;
         p5.stroke(0);
-        p5.fill(r);
+        p5.fill(r,g,b);
         p5.rect(x*scl,y*scl,scl,scl);
       }
     }
@@ -40,13 +42,38 @@ export const Area_Turtle_Visualizer = new Visualizer(
   
       p5.fill(p5.random(225),p5.random(225),p5.random(225))
 
-      const Radius = height * amplitude/2;
-      const beepSize = height * amplitude/2;
+      // const Radius = height * amplitude/4;
+      // const Size = height * amplitude/4;
+
+      // const bassX = p5.noise(p5.millis() / 1000) * width;
+			// const bassY = p5.noise(i / 100) * height;
+
+      // const beepX = p5.noise(p5.millis() / 500) * width;
+			// const beepY = p5.noise(i / 50) * height;
       
-      p5.ellipse(p5.random(width), p5.random(height), Radius, Radius);
-      p5.rect(p5.random(width), p5.random(height), beepSize, beepSize);
+      // p5.ellipse(p5.random(width), p5.random(height), Radius, Radius);
+      // p5.rect(p5.random(width), p5.random(height), Size, Size);
+
+      //p5.ellipse(bassX, bassY, Radius, Radius);
+      //p5.rect(beepX, beepY, Size, Size);
     p5.endShape()
     }
-   
+    phase +=1
+    const amplitude = values[0] as number;
+    const Radius = height * amplitude;
+    const Size = height * amplitude;
+
+    const bassX = p5.noise(p5.millis() / 1000) * width;
+		const bassY = p5.noise(phase / 100) * height;
+
+    const beepX = p5.noise(p5.millis() / 500) * width;
+		const beepY = p5.noise(phase / 50) * height;
+      
+    p5.ellipse(p5.random(width), p5.random(height), Radius, Radius);
+    p5.rect(p5.random(width), p5.random(height), Size, Size);
+
+    p5.ellipse(bassX, bassY, Radius, Radius);
+    p5.rect(beepX, beepY, Size, Size);
+    
   },
 );
