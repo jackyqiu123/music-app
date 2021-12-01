1641ad2675f5120620df90f5ae63a9d0eed69ac5
Jacky Qiu <jaqiu123@gmail.com>
2021-11-30 16:53:29 -0800

    added different visualizer, removed tornado

--- client/src/visualizers/Jackyqiu123.tsx
+++ client/src/visualizers/Jackyqiu123.tsx
@@ -6,60 +6,31 @@ import { SideNav } from '../SideNav';
 // project imports
 import { Visualizer } from '../Visualizers';
 
-// export const TriangleVisualizer = new Visualizer(
-//     'CylindricalWave',
-//     (p5: P5, analyzer: Tone.Analyser) => {
-//         const width = window.innerWidth;
-//         const height = window.innerHeight / 2;
-//         const dim = Math.min(width, height);
-
-//         p5.background(0, 0, 0, 255);
-
-//         p5.strokeWeight(dim * 0.01);
-//         p5.stroke(255, 255, 255, 255);
-//         p5.noFill();
-
-//         const values = analyzer.getValue();
-//         p5.beginShape();
-//         for (let i = 0; i < 200; i++) {
-//             const amplitude = values[i] as number;
-//             for(let j = 0; j < 200; j++){
-//                 // let x = p5.map(i, j, 199, 0,width)
-//                 // let y = height / 2 + amplitude * height;
-//                 let x = height / 2 + amplitude * width;
-//                 let y = p5.map(i, j, 199, 0,height);
-              
-//                 p5.vertex(x+380,y);
-//             }
-//         }
-//         p5.endShape();
-//     },
-// );
 export const TriangleVisualizer = new Visualizer(
-    'Tornado',
+    'InfinityWave',
     (p5: P5, analyzer: Tone.Analyser) => {
         const width = window.innerWidth;
         const height = window.innerHeight / 2;
         const dim = Math.min(width, height);
 
         p5.background(0, 0, 0, 255);
-
-        p5.strokeWeight(dim * 0.01);
+        // p5.strokeWeight(dim * 0.01);
         p5.stroke(255, 255, 255, 255);
         p5.noFill();
 
+        p5.translate(width/2, height/2);
         const values = analyzer.getValue();
         p5.beginShape();
-        for (let i = 0; i < 200; i++) {
-            const amplitude = values[i] as number;
-            for(let j = 0; j < 50; j++){
-                let x = height / 2 + amplitude * width;
-                let y = p5.map(i, j, 199, 0, height);
-              
-                p5.vertex(x+380,y);
-            }
+
+        for(let i = 0; i < 359; i++){
+            let index = Math.floor(p5.map(values[i] as number, 0, 180,0,values.length -1));
+            let r = p5.map(index * 2, -1, 1, 100, 200);
+            let x = r * Math.cos(i*5);
+            let y = r * Math.sin(i*10);
+            p5.vertex(x - 100, y);
         }
         p5.endShape();
+        
     },
 );
 export const Circle = new Visualizer(
