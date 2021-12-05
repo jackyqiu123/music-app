68d2dd18b90d815c54f171475e6dd871bfe7ba6a
Zach <60633052+Area-Turtle@users.noreply.github.com>
2021-12-04 18:41:37 -0800

    updated

--- client/src/visualizers/Area-Turtle_Visualizer.tsx
+++ client/src/visualizers/Area-Turtle_Visualizer.tsx
@@ -9,17 +9,39 @@ import { Visualizer } from '../Visualizers';
 var phase = 0
 var t = 0;
 
+
 export const Area_Turtle_Visualizer = new Visualizer(
   'Area-Turtle_Visualizer',
   (p5: P5, analyzer: Tone.Analyser) => {
     
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
+
+    
     const values = analyzer.getValue();
-    p5.background(255);
-    /**
-     * background weird thing
-     */
+    // p5.background(255);
+    // /**
+    //  * background weird thing
+    //  */
+    // p5.translate(width/2, height/2);
+    // p5.background(0, 0, 0, 255);
+    // //p5.angleMode(p5.DEGREES);
+    
+    // // p5.strokeWeight(dim * 0.01);
+    // p5.stroke(255);
+    // p5.noFill();
+
+    // const values = analyzer.getValue();
+    // p5.beginShape();
+    //     for(let i = 0; i <= 90; i++){
+    //         let index = Math.floor(p5.map(values[i] as number, 0, 180,0,values.length -1));
+    //         let r = p5.map(values[index] as number, -1, 1, 150,350)
+    //         let x = r * Math.sin(i);
+    //         let y = r * Math.cos(i);
+    //         p5.vertex(x - 200,y)
+    //     }
+    // p5.endShape();
+
     var inc = 0.01;
     let scl = 20;
     let cols = width/scl;
